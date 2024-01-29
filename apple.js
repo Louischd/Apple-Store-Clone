434cf3f64ae8afc5b6d9e2a26db055b107cfc8a3
Louischd <louiscochard2@gmail.com>
2024-01-29 15:48:22 +0100

    1ere version

---
+++ apple.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {
+        coverTrigger: false
+    });
+});
+const links = document.querySelectorAll('.search p');
+
